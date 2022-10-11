e443df926b74a88f69a5688e35a3af90a9f45784
vanhieuhp <vanhieuit10@gmail.com>
2022-10-11 16:53:56 +0700

    change file

--- src/main/java/com/jenkins/HelloJenkinsApplication.java
+++ src/main/java/com/jenkins/HelloJenkinsApplication.java
@@ -10,14 +10,14 @@ public class HelloJenkinsApplication {
 
         SpringApplication.run(HelloJenkinsApplication.class, args);
         System.out.println("Greetings Workshop!");
-        System.out.println("This is an example file");
-        System.out.println("for use in workshop exercises.");
-        System.out.println("This is on the master branch");
-        System.out.println("This file is written in Java.");
-        System.out.println("Isn't this exciting?");
-        System.out.println("Maybe?");
-        System.out.println("Okay then.");
-        System.out.println("Goodbye Workshop!");
+//        System.out.println("This is an example file");
+//        System.out.println("for use in workshop exercises.");
+//        System.out.println("This is on the master branch");
+//        System.out.println("This file is written in Java.");
+//        System.out.println("Isn't this exciting?");
+//        System.out.println("Maybe?");
+//        System.out.println("Okay then.");
+//        System.out.println("Goodbye Workshop!");
     }
 
 }
