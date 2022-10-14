9d4749d6eab7749c36ef813f225aa547462d2fb1
= <=>
2022-10-14 15:29:15 +0700

    hello

--- src/main/java/com/vanhieu/HelloWorldApplication.java
+++ src/main/java/com/vanhieu/HelloWorldApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
-        System.out.println("hello Gradle");
+        System.out.println("hello Gradle - test");
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
